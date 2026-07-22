030aeade44edb4d5503dbacd136436f2f0ee8eab
Glody Ngoma <glodyngoma64@gmail.com>
2026-07-22 14:16:34 +0100

    fix(communication): restore dependency injection and authentication

--- backend/src/communication/communication.module.ts
+++ backend/src/communication/communication.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { CommunicationController } from './communication.controller';
 import { CommunicationService } from './communication.service';
 import { DocumentResolverService } from './document-resolver.service';
+import { DatabaseModule } from '../database/database.module';
 import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [EmailModule],
+  imports: [DatabaseModule, EmailModule],
   controllers: [CommunicationController],
   providers: [CommunicationService, DocumentResolverService],
   exports: [CommunicationService],
